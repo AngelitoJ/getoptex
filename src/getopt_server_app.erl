245b834a98a72be616b5412e92dea09bde401edd
AngelitoJ <angeljalvarezmiguel@gmail.com>
2014-10-21 13:25:25 +0200

    Initial application main file
    Copy-pasted from my Hydra project removing unneeded bits here and there

---
+++ src/getopt_server_app.erl
@@ -0,0 +1,34 @@
+%% GetOptEx and enhanced getopt application
+%% (C) 2014 Angel J. Alvarez Miguel
+
+
+
+-module(getoptex_server_app).
+-author("angeljalvarezmiguel@gmail.com").
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, StartArgs) ->
+
+%%   {ok, CmdlineOpts} = application:get_env(cmdline_options),    %% recover Opts from the enviroment
+%%   AppOptions = [{start_args,StartArgs}|CmdlineOpts],           %% Combine start aargs with cmdline options
+
+   case top_sup:start_link(StartArgs) of
+   {ok, Pid} ->
+       io:format("Application started...\n\n"),
+      {ok, Pid};
+   {error,Other} ->
+      io:format("Application crashed! Error: ~p\n",[Other]),
+      {error, Other}
+   end.
+
+stop(_State) ->
+    io:format("Application stopped...\n\n"),
+    ok.
+
