8059d3c90238ec80cc38a7d5de65341a7b707d09
AngelitoJ <angeljalvarezmiguel@gmail.com>
2014-11-01 21:24:17 +0100

    Some refactorings..

--- src/getoptex.erl
+++ src/getoptex.erl
@@ -10,45 +10,49 @@
 -author("angeljalvarezmiguel@gmail.com").
 -compile(export_all).
 
+-import(lists, [foldl/3]).
+
 
 % Parse cmdline args with getopt and then checkout additional conditions on requested options
 parse_args(SpecList, ArgList, FunList) ->
     case getopt:parse(SpecList, ArgList) of
-    {ok, {Options, NonOptArgs}} ->                                                       %% Successfull getopt 
-        COpts = lists:foldl(fun(Fun,Acc) when is_function(Fun,1), is_list(Acc) -> 
-                                        Fun(Acc);
-                                (_F,Acc) -> Acc end
-                                ,Options                                                 %% Options returned from getopt
-                                ,FunList                                                 %% List of funs collected
-                            ),
-        case is_list(COpts) of
-        true ->
-            {ok,{COpts,NonOptArgs}};                                                     %% make a ok result out of an Opts list
-        false ->
-            COpts                                                                        %% Oh boy! this is a error tuple...
-        end;
-    {error, {Reason, Data}} -> 
-        {error, {Reason, Data}}                                                          %% getopt returned some error
+    {ok, {Options, NonOptArgs}} -> 
+                                    check_args(Options, NonOptArgs, FunList);           %% Successfull getopt
+    {error, {Reason, Data}} ->
+                                    {error, {Reason, Data}}                             %% getopt returned some error
     end.
+
+check_args(Options, NonOptArgs, FunList) ->
+    case foldl(fun do_check/2, Options, FunList) of
+    COpts when is_list(COpts) -> 
+                                {ok,{COpts, NonOptArgs}};                               %% make a ok result out of an Opts list
+    Other                     ->
+                                Other                                                   %% Oh boy! this is a error tuple...
+    end.
+
+%% apply a checking fun 
+do_check(Fun,Acc) when is_function(Fun,1), is_list(Acc) -> Fun(Acc);
+do_check(_,Acc) -> Acc.
 
 
 %% Fold over a module list accumulating optspecs and funs exported
 collect_option_providers(Modules) when is_list(Modules) ->
-    {Opts, Funs} = lists:foldl(fun(Mod,{OptList,FunList}) ->
+    {Opts, Funs} = foldl(fun(Mod,{OptList,FunList}) ->
                                             {ModOpts,ModFuns} = Mod:option_specs(),
                                             {ModOpts ++ OptList, ModFuns ++ FunList} end
-                                ,{[],[]}
-                                ,Modules),
+                        ,{[],[]}
+                        ,Modules),
     {lists:reverse(Opts), lists:reverse(Funs)}.
 
-%% Old code, really ugly
+% get modules composing a given application
+-spec list_app_modules(atom(),atom(),atom()) -> [atom() | tuple()].
+list_app_modules(App,Class,Key) when is_atom(App), is_atom(Class), is_atom(Key) ->
+    {ok,ModuleList}  = application:get_key(App,modules),
+    list_modules(Class,Key,ModuleList).
 
-%% collect_option_providers(ModuleList) ->
-%%    collect_option_providers([],[],ModuleList). 
-
-%% collect_option_providers(Opts,Funs,[]) ->
-%%        {lists:reverse(Opts),lists:reverse(Funs)};
-
-%% collect_option_providers(Opts,Funs,[Module|RestList]) ->
-%%    {NewOpts,NewFuns} = Module:option_specs(),
-%%    collect_option_providers(NewOpts ++ Opts,NewFuns ++ Funs,RestList).
+% get modules exporting a value
+-spec list_modules(atom(),atom(),[atom() | tuple()]) -> [atom() | tuple()].
+list_modules(Class,Key,AppModules) when is_atom(Class),is_atom(Key) ->
+    % Qué modulos exportan una valor de la clase pedida?
+    Modules = [ X || X <- AppModules, lists:keymember(Key,1,X:module_info(Class)) ],
+    Modules.
